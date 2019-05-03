a87bf9efb94fc5f948fe852753a75049b63b1286
saaucorp <shuhaila@springrainit.com>
2019-05-03 21:50:47 +0600

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# [ExpressTransferNetwork](https://github.com/shuhailshuvo/ExpressTransferNetwork)
-Web Application for Express Transfer Network 
---
+++ precache-manifest.713e23a4ef279ccd90465ac4edf6673f.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/ExpressTransferNetwork/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "5dfecca36605cfab7249",
+    "url": "/ExpressTransferNetwork/static/css/main.5ee92822.chunk.css"
+  },
+  {
+    "revision": "51dab8d45c08f1a38bc1",
+    "url": "/ExpressTransferNetwork/static/js/runtime~main.51dab8d4.js"
+  },
+  {
+    "revision": "b578262ceb5d1a4e0776",
+    "url": "/ExpressTransferNetwork/static/js/2.b578262c.chunk.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/ExpressTransferNetwork/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/ExpressTransferNetwork/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/ExpressTransferNetwork/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/ExpressTransferNetwork/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/ExpressTransferNetwork/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "390d9f58ce656c65a7a626d7a1e630f9",
+    "url": "/ExpressTransferNetwork/static/media/1.390d9f58.png"
+  },
+  {
+    "revision": "fb9ceb4b413f65ab9dec2816c0e20942",
+    "url": "/ExpressTransferNetwork/static/media/2.fb9ceb4b.png"
+  },
+  {
+    "revision": "d803e7607aaf635bbbea138fe73d4ebb",
+    "url": "/ExpressTransferNetwork/static/media/3.d803e760.png"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/ExpressTransferNetwork/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/ExpressTransferNetwork/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/ExpressTransferNetwork/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/ExpressTransferNetwork/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/ExpressTransferNetwork/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "5dfecca36605cfab7249",
+    "url": "/ExpressTransferNetwork/static/js/main.5dfecca3.chunk.js"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/ExpressTransferNetwork/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "3718ee69bd06eb1cafdd143a67b07dc5",
+    "url": "/ExpressTransferNetwork/static/media/4.3718ee69.png"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/ExpressTransferNetwork/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "7bf89c69f69202e110ba02ea621a4a7c",
+    "url": "/ExpressTransferNetwork/static/media/logo.7bf89c69.png"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/ExpressTransferNetwork/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/ExpressTransferNetwork/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/ExpressTransferNetwork/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/ExpressTransferNetwork/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/ExpressTransferNetwork/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "e141bc5cc095cd86eaa12712c51fe93b",
+    "url": "/ExpressTransferNetwork/static/media/loading.e141bc5c.gif"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/ExpressTransferNetwork/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "a3d371e422723d7c173694954090bb07",
+    "url": "/ExpressTransferNetwork/static/media/4.a3d371e4.png"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/ExpressTransferNetwork/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/ExpressTransferNetwork/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "20980b10e8dcb59c20a7357a1717aef6",
+    "url": "/ExpressTransferNetwork/static/media/landing-bg.20980b10.png"
+  },
+  {
+    "revision": "2151ab9796f42cde4ef3e85f326d68da",
+    "url": "/ExpressTransferNetwork/index.html"
+  }
+];
